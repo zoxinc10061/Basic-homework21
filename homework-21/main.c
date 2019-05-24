64a49202c05e0a9931296bb74a4bb2ef51f7c7ff
zoxinc10061 <48340647+zoxinc10061@users.noreply.github.com>
2019-05-24 10:45:44 +0800

    Add files via upload

---
+++ homework-21/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h> 
+
+#include <stdlib.h>
+
+struct servitor
+
+{
+
+   char id[8],name[12];
+
+   int hourly_pay,hours,payment;
+
+};
+
+void calc(struct servitor *);
+
+int main(void)
+
+{
+
+   struct servitor emp; 
+
+   printf("-------------------------------------------------\n");
+
+   printf("                 Servitor工讀生結構體                 \n");
+
+   printf("-------------------------------------------------\n"); 
+
+   printf("請輸入編號,姓名,時薪,工作時數: ");
+
+   scanf(" %s %s %d %d", emp.id, emp.name, &emp.hourly_pay, &emp.hours);
+
+   calc(&emp);
+
+   printf("月收入: %d\n", emp.payment);
+
+   system("pause"); 
+
+   return 0;
+
+}
+
+
+
+void calc(struct servitor *p)
+
+{
+
+    p->payment=p->hourly_pay*p->hours;
+
+}
